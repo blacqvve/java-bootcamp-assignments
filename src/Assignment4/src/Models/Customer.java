585c839d01b5495147d84bd0950fa83f06939a06
Yiğitcan Öksüz <yigitcanoksuz@gmail.com>
2021-05-03 20:20:05 +0300

    customer entity added

---
+++ src/Assignment4/src/Models/Customer.java
@@ -0,0 +1,57 @@
+package Assignment4.src.Models;
+
+import java.util.Date;
+
+public class Customer {
+    private int id;
+    private String firstName;
+    private String lastName;
+    private Date dateOfBirth;
+    private String nationalityId;
+
+
+    public int getId() {
+        return this.id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return this.firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return this.lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public Date getDateOfBirth() {
+        return this.dateOfBirth;
+    }
+
+    public void setDateOfBirth(Date dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public String getNationalityId() {
+        return this.nationalityId;
+    }
+
+    public void setNationalityId(String nationalityId) {
+        this.nationalityId = nationalityId;
+    }
+
+    public String getFullName(){
+        return this.firstName + " " + this.lastName;
+    }
+    
+}
