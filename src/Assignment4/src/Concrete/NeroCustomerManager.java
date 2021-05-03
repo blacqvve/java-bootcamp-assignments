c71148694082bad1a5c0cfa7c62ea12a64c3d0f3
Yiğitcan Öksüz <yigitcanoksuz@gmail.com>
2021-05-03 20:19:51 +0300

    concrete services added

---
+++ src/Assignment4/src/Concrete/NeroCustomerManager.java
@@ -0,0 +1,14 @@
+package Assignment4.src.Concrete;
+
+import Assignment4.src.Interfaces.CustomerService;
+import Assignment4.src.Models.Customer;
+
+public class NeroCustomerManager implements CustomerService{
+
+    @Override
+    public void Save(Customer customer) {
+        // TODO Auto-generated method stub
+        
+    }
+    
+}
