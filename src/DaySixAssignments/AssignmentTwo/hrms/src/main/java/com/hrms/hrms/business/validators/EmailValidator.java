9437b0b66771da468f6e7004fc9101ac2768f9e4
Yiğitcan Öksüz <yigitcanoksuz@gmail.com>
2021-05-28 19:24:45 +0300

    email validator changes

--- src/DaySixAssignments/AssignmentTwo/hrms/src/main/java/com/hrms/hrms/business/validators/EmailValidator.java
+++ src/DaySixAssignments/AssignmentTwo/hrms/src/main/java/com/hrms/hrms/business/validators/EmailValidator.java
@@ -2,6 +2,7 @@ package com.hrms.hrms.business.validators;
 
 import java.util.regex.Pattern;
 
+import com.hrms.hrms.core.utilities.validators.EmailRegexValidator;
 import com.hrms.hrms.core.utilities.validators.abstracts.Validator;
 
 import org.springframework.stereotype.Component;
@@ -12,22 +13,10 @@ import java.util.regex.Matcher;
 @Component("emailValidator")
 public class EmailValidator implements Validator<String> {
 
-    public static final Pattern VALID_EMAIL_ADDRESS_REGEX = Pattern.compile(
-            "[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?",
-            Pattern.CASE_INSENSITIVE);
-
     @Override
-    public boolean validate(String object) {
+    public boolean validate(String emailAdress) {
 
-        Matcher matcher = VALID_EMAIL_ADDRESS_REGEX.matcher(object);
-
-        boolean matchFound = matcher.find();
-
-        String validationText = matchFound ? "valid" : "invalid";
-
-        System.out.println(object + " is a " + validationText + " mail account");
-
-        return matchFound;
+        return EmailRegexValidator.validateEmail(emailAdress);
     }
 
 }
--- src/DaySixAssignments/AssignmentTwo/hrms/src/main/java/com/hrms/hrms/core/utilities/validators/EmailRegexValidator.java
+++ src/DaySixAssignments/AssignmentTwo/hrms/src/main/java/com/hrms/hrms/core/utilities/validators/EmailRegexValidator.java
@@ -1,6 +1,24 @@
 package com.hrms.hrms.core.utilities.validators;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public  class EmailRegexValidator {
     
-    public static boolean validateEmail(String email)
+    public static final Pattern VALID_EMAIL_ADDRESS_REGEX = Pattern.compile(
+            "[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?",
+            Pattern.CASE_INSENSITIVE);
+
+            
+    public static boolean validateEmail(String email){
+        Matcher matcher = VALID_EMAIL_ADDRESS_REGEX.matcher(email);
+
+        boolean matchFound = matcher.find();
+
+        String validationText = matchFound ? "valid" : "invalid";
+
+        System.out.println(email + " is a " + validationText + " mail account");
+
+        return matchFound;
+    }
 }
